cc8d7b1b2c5af7921bb6682cebfe5d6f90c93bf3
Tapansahu <sahutapan320@gmail.com>
2024-08-03 14:13:28 +0530

    feat:blog 1

--- src/modules/Blog/components/Image.jsx
+++ src/modules/Blog/components/Image.jsx
@@ -7,7 +7,9 @@ const Image = ({
   console.log(images);
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+    // TODO: if required two images in future the change it
+    // <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+    <div className="flex items-center justify-center">
       {images?.map((image, index) => (
         <img
           key={index}

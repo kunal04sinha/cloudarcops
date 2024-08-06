625d5399039e99e3558deb4b06d52cc112522a02
kunalsinha04 <kunal.sinha32@gmail.com>
2024-08-06 22:48:51 +0530

    Changes on resource and blog 5,6 added

--- src/modules/Resources/components/CardsDeatil.jsx
+++ src/modules/Resources/components/CardsDeatil.jsx
@@ -3,64 +3,13 @@ import Card from "./Card";
 import { Link } from "react-router-dom";
 import { paths } from "../../../const/path";
 import img from "../../../assets/images/MigratingfromECStoEKS-2-.avif";
-
-const cardsData = [
-  {
-    id: 1,
-    title:
-      "Comprehensive Kubernetes Monitoring with Self-Hosted Grafana: A Case Study",
-    imageUrl: img,
-    description:
-      "In the world of enterprise IT, effective monitoring of infrastructure is a critical task. Ensuring that Kubernetes clusters and their components are operating efficiently can be a challenge,especially at scale. In this blog .....",
-    buttonText: "Read More",
-    buttonUrl: "#",
-  },
-  {
-    id: 2,
-    title:
-      "Cost-Efficiency and Automation with Serverless Architecture for Enterprise Customers",
-    imageUrl: img,
-    description:
-      "In the ever-evolving landscape of cloud computing, achieving cost-efficiency and seamless automation is paramount for enterprise customers. Recently, I successfully implemented a serverless architecture using AWS services such ......",
-    buttonText: "Read More",
-    buttonUrl: "#",
-  },
-  {
-    id: 3,
-    title:
-      "Revolutionizing Web Applications: A Dive into My Three-Tier Architecture with AWS CloudFront",
-    imageUrl: img,
-    description:
-      "In the evolving landscape of web development, the need for robust, scalable, and efficient architectures has never been more critical. As a seasoned DevOps engineer, I recently embarked on a journey to develop a sophisticated.....",
-    buttonText: "Read More",
-    buttonUrl: "#",
-  },
-  {
-    id: 4,
-    title:
-      "Enhancing Fintech Infrastructure with AWS EKS and Karpenter: A Journey to Scalability and Reliability",
-    imageUrl: img,
-    description:
-      "In today's rapidly evolving fintech landscape, ensuring that your application infrastructure is both scalable and reliable is paramount. For [Fintech Company], this meant rethinking and optimizing their existing setup .......",
-    buttonText: "Read More",
-    buttonUrl: "#",
-  },
-  {
-    id: 5,
-    title: "Migration from EC2 to EKS",
-    imageUrl: img,
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
-    buttonText: "Read More",
-    buttonUrl: "#",
-  },
-];
+import { data } from "../data.js";
 
 const CardsDetail = () => {
   return (
     <div className="container mx-auto px-4 py-12 px-12">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4 gap-10">
-        {cardsData.map((card, index) => (
+        {data.map((card, index) => (
           <Card
             id={card.id}
             key={index}
---
+++ src/modules/Resources/data.js
@@ -0,0 +1,67 @@
+import cloudMonitoring from "../../assets/images/CloudArcOps-Monitoring.jpg";
+import domainServerLess from "../../assets/images/domain-serverless.jpg";
+import thirdImg from "../../assets/images/cloudarcops-three-tear.drawio.png";
+import fourthImg from "../../assets/images/image-eks with autoscaking of nodes using karpenter.png";
+import automation from "../../assets/images/automation.png";
+import sixth from "../../assets/images/sixth.png";
+
+export const data = [
+  {
+    id: 1,
+    title:
+      "Comprehensive Kubernetes Monitoring with Self-Hosted Grafana: A Case Study",
+    imageUrl: cloudMonitoring,
+    description:
+      "In the world of enterprise IT, effective monitoring of infrastructure is a critical task. Ensuring that Kubernetes clusters and their components are operating efficiently can be a challenge, especially at scale. In this blog .....",
+    buttonText: "Read More",
+    buttonUrl: "#",
+  },
+  {
+    id: 2,
+    title:
+      "Cost-Efficiency and Automation with Serverless Architecture for Enterprise Customers",
+    imageUrl: domainServerLess,
+    description:
+      "In the ever-evolving landscape of cloud computing, achieving cost-efficiency and seamless automation is paramount for enterprise customers. Recently, I successfully implemented a serverless architecture using AWS services such ......",
+    buttonText: "Read More",
+    buttonUrl: "#",
+  },
+  {
+    id: 3,
+    title:
+      "Revolutionizing Web Applications: A Dive into My Three-Tier Architecture with AWS CloudFront",
+    imageUrl: thirdImg,
+    description:
+      "In the evolving landscape of web development, the need for robust, scalable, and efficient architectures has never been more critical. As a seasoned DevOps engineer, I recently embarked on a journey to develop a sophisticated.....",
+    buttonText: "Read More",
+    buttonUrl: "#",
+  },
+  {
+    id: 4,
+    title:
+      "Enhancing Fintech Infrastructure with AWS EKS and Karpenter: A Journey to Scalability and Reliability",
+    imageUrl: fourthImg,
+    description:
+      "In today's rapidly evolving fintech landscape, ensuring that your application infrastructure is both scalable and reliable is paramount. For [Fintech Company], this meant rethinking and optimizing their existing setup .......",
+    buttonText: "Read More",
+    buttonUrl: "#",
+  },
+  {
+    id: 5,
+    title: "Creating a Streamlined DevOps Pipeline: From Commit to Cloud",
+    imageUrl: automation,
+    description:
+      "In the fast-evolving landscape of software development, the need for speed and reliability in deploying applications is paramount. My recent project, which involved automating the application deployment process ....",
+    buttonText: "Read More",
+    buttonUrl: "#",
+  },
+  {
+    id: 6,
+    title: "Building a Scalable and Resilient Media Platform with AWS",
+    imageUrl: sixth,
+    description:
+      "In today's digital age, the demand for fast, reliable, and scalable web services is paramount. We recently had the opportunity to architect and deploy a sophisticated media platform leveraging AWS's vast ecosystem.",
+    buttonText: "Read More",
+    buttonUrl: "#",
+  },
+];

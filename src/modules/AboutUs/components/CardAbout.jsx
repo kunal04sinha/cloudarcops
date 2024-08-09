f32b29118a667895e27701610fc2d917cd261906
kunalsinha04 <kunal.sinha32@gmail.com>
2024-08-09 15:42:41 +0530

    chnages in navbar (name in sub menu) and added italic in about us

--- src/modules/AboutUs/components/CardAbout.jsx
+++ src/modules/AboutUs/components/CardAbout.jsx
@@ -4,9 +4,9 @@ const CardAbout = ({ name, para1, para2, para3, founder }) => {
   return (
     <div className="bg-white w-full shadow-2xl mt-6 rounded-lg overflow-hidden">
       <div className="p-4">
-        <p className="text-gray-700 font-semibold mb-5">{para1}</p>
-        <p className="text-gray-700 font-semibold mb-5">{para2}</p>
-        <p className="text-gray-700 font-semibold mb-4">{para3}</p>
+        <p className="text-gray-700 font-semibold mb-5 italic">{para1}</p>
+        <p className="text-gray-700 font-semibold mb-5 italic">{para2}</p>
+        <p className="text-gray-700 font-semibold mb-4 italic">{para3}</p>
         <h3 className="text-lg text-right ">{founder}</h3>
         <p className="text-blue-600 font-bold text-right">{name}</p>
       </div>
